bb048f8e3bc878d89835f4497826fbb19b8cc11e
aklademarco <aklademarco2@gmail.com>
2025-04-18 20:34:41 +0000

    initial commit

---
+++ personal-portfolio/script.js
@@ -0,0 +1,35 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     const aboutSection = document.getElementById("about");
+//     const homeSection = document.querySelector(".home");
+//     const educationSection = document.getElementById("education");
+//     const aboutLink = document.querySelector(".navbar a[href='#about']");
+//     const educationLink = document.querySelector(".navbar a[href='#education']");
+//     const homeLink = document.querySelector(".navbar a[href='#home']");
+
+//     aboutLink.addEventListener("click", (e) => {
+//         e.preventDefault();
+//         homeSection.style.display = "none";
+//         aboutSection.style.display = "flex";
+//         educationSection.style.display = "none";
+//     });
+
+//     educationLink.addEventListener("click", (e) => {
+//         e.preventDefault();
+//         homeSection.style.display = "none";
+//         aboutSection.style.display = "none";
+//         educationSection.style.display = "flex";
+//     });
+
+//     if (homeLink) {
+//         homeLink.addEventListener("click", (e) => {
+//             e.preventDefault();
+//             aboutSection.style.display = "none";
+//             educationSection.style.display = "none";
+//             homeSection.style.display = "flex";
+//         });
+   
+//     }
+
+  
+// });
+
